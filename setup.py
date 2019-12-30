77fe1ba8da17c5beae052a9b77dba530f53eb096
krzesu0 <przemekbarcicki@gmail.com>
2019-12-30 15:37:42 +0000

    Working on docker image for my other project. Need package.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="betterplan",
+    description="Webscraper for lesson plan scheduler made by Vulcan.",
+    author="krzesu0",
+    url="https://github.com/krzesu0/betterplan",
+    packages=["betterplan"],
+    package_dir={"betterplan": "src/"}
+    )
